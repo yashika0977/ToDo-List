4bd9a4ad43a5839b50cc7c201d14783efb41f4d6
Yashika <yashikagaur156@gmail.com>
2025-07-21 14:36:33 +0530

    Initial commit

---
+++ todo/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [task, setTask] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTask = () => {
+    if (task.trim() !== '') {
+      setTodos([...todos, task]);
+      setTask('');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>📝 To-Do List</h1>
+      <div style={{ marginBottom: '20px' }}>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter a task"
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
+
+      <ul>
+        {todos.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
